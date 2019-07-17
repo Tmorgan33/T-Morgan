55bb6438844b9ec1b6effc2deece1c84f4910282
Tmorgan33 <Ms.Terry.L.Morgan@gmail.com>
2019-07-17 16:04:52 -0400

    fixed some merge conflicts

--- function/func.js
+++ function/func.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use script"
         var num1 = 598;
          num2 = 654;{
@@ -6,12 +5,3 @@
                 
 console.log (num1 * num2);
 
-=======
-"use script"
-        var num1 = 598;
-         num2 = 654;{
-    }
-                
-console.log (num1 * num2);
-
->>>>>>> ce437ab9f5a88b6727e69bdf3ebc9f0dfe33ef2b
