f02e2425247823b35eefe43e50f418e2160af11d
Tmorgan33 <Ms.Terry.L.Morgan@gmail.com>
2019-07-23 16:37:52 -0400

    working on a menu

---
+++ customer/food.js
@@ -0,0 +1,19 @@
+"use script"
+
+var menu= ["fries", "10",
+            "shakes", "15",
+            "hotdogs", "20"  ]
+            var input=document.querySelector ("input")
+            var button=document.querySelector ("button")
+button.addEventListener ("click", function () {
+    for (var i in menu){
+    console.log(menu[i]);
+    }
+
+
+
+
+
+
+
+})
