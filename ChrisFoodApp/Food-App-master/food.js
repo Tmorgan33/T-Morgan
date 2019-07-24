4666101bdebcf9748b9bbd9b6e48241e60b12e4d
Tmorgan33 <Ms.Terry.L.Morgan@gmail.com>
2019-07-24 19:19:58 -0400

    Tuesday 3 person team food app

---
+++ ChrisFoodApp/Food-App-master/food.js
@@ -0,0 +1,80 @@
+console.log("hey how are you");
+window.onload = function(){
+
+var button = document.querySelector("button");
+var input = document.querySelector("input");
+
+
+
+var menu = {"Fries": 10, 
+			 "Shakes": 20,
+			 "BaconBite": 50, 
+			 "Chicken": 45, 
+			 "Fruit Bowl": 9, 
+			 "Chips": 29, 
+			 "Hot Dogs": 104, 
+			 "Salad": 56, 
+			 "Pizza": 29, 
+			 "Coke": 18}
+
+
+
+button.addEventListener("click", function(){
+
+var customerOrder = input.value;
+var itemPair = customerOrder.split(", ");
+
+for(var i = 0; i < itemPair.length; i++){
+	var items = itemPair[i].split(": ");
+var menuItems = Object.keys(menu)
+var menuQuan = Object.values(menu)
+for(counter=0; counter < menuItems.length; counter++){
+	// if(menuItems[counter] =! items[0]){
+	// 	console.log("Sorry we don't carry that item!")
+	// }
+	if (menuItems[counter] == items[0]) {
+	var remainder = menuQuan[counter] - items[1];
+	console.log(remainder);
+	}
+	// console.log();
+
+// if (items[0] === menuItems){
+// console.log("yeah");
+// console.log(items[0]);
+// console.log(menuItems);
+// console.log(items[0] === menuItems);
+  }
+// }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// if (items[0] )
+
+
+
+});
+
+
+
+
+
+
+
+};
+
+
+// for (var fastFood in menu){
+// 	console.log(fastFood);
+// 	console.log(menu[fastFood]);
+	
+// }
